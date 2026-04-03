7e5305a5ea7410190aecd9bd8156afc5bef39880
Yanzz231 <mrpartys05@gmail.com>
2026-04-03 21:06:56 +0700

    feat: add icon components

---
+++ src/assets/icon/EyeOffIcon.tsx
@@ -0,0 +1,10 @@
+import type { SVGProps } from "react";
+
+export const EyeOffIcon = (props: SVGProps<SVGSVGElement>) => (
+  <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" {...props}>
+    <path d="M10.733 5.076a10.744 10.744 0 0 1 1.267-.076c7.498 0 10 7 10 7a17.16 17.16 0 0 1-2.008 3m-4.66-1.574a3 3 0 0 1-4.04-4.043"/>
+    <path d="m2 2 20 20"/>
+    <path d="M14.121 14.121a3 3 0 0 1-4.242-4.242"/>
+    <path d="M4.09 5.862C2.65 7.42 2 9 2 9s2.5 7 10 7c1.378 0 2.657-.282 3.824-.766"/>
+  </svg>
+);
---
+++ src/assets/icon/index.ts
@@ -0,0 +1,6 @@
+export { InstagramIcon } from "./InstagramIcon";
+export { TwitterIcon } from "./TwitterIcon";
+export { LinkedInIcon } from "./LinkedInIcon";
+export { GoogleIcon } from "./GoogleIcon";
+export { EyeIcon } from "./EyeIcon";
+export { EyeOffIcon } from "./EyeOffIcon";
